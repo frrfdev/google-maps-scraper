ed6c69011eddc65c1e49626ca62db00d38ca3961
Felipe Renan <64810899+frrfdev@users.noreply.github.com>
2025-01-04 19:28:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.get("/search", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: "/root/.cache/puppeteer/chrome-headless-shell", // Caminho para o Chrome instalado
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
